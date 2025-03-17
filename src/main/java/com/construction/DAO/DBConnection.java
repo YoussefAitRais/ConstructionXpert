29322fd7e682ab462949647c78f67d36ef1a8312
Youssef Ait Rais <copybuisness007@gmail.com>
2025-03-17 12:16:38 +0100

    DBconnection

--- src/main/java/com/construction/DAO/DBConnection.java
+++ src/main/java/com/construction/DAO/DBConnection.java
@@ -1,4 +1,30 @@
 package com.construction.DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static Connection connection;
+
+    public static final String URL = "jdbc:mysql://localhost:3306/construction;";
+    public static final String USER = "root";
+    public static final String PASSWORD = "admin";
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver"); // Charger le driver MySQL
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Connexion réussie à la base de données !");
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+                System.out.println("Erreur de connexion à la base de données !");
+            }
+        }
+        return connection;
+    }
+
+
+
 }
