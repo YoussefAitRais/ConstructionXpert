8ee560925cafecb61a44c4412f58431d020c8a30
Youssef Ait Rais <copybuisness007@gmail.com>
2025-03-17 15:51:33 +0100

    servlet initalisation

--- src/main/java/com/construction/Web/ProjetServlet.java
+++ src/main/java/com/construction/Web/ProjetServlet.java
@@ -1,4 +1,27 @@
 package com.construction.Web;
 
-public class ProjetServlet {
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet
+public class ProjetServlet extends HttpServlet {
+
+    @Override
+    public void init () throws ServletException {
+        super.init();
+    }
+
+    protected void doGet (HttpServletRequest request , HttpServletResponse response)
+        throws  ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+
+    }
+
+
+
 }
