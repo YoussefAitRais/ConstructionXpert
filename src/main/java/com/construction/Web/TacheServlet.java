5f0b9b73f0a3721bad46f2e1b90e547efd89491c
Youssef Ait Rais <copybuisness007@gmail.com>
2025-03-20 14:14:57 +0100

    servlet tache

---
+++ src/main/java/com/construction/Web/TacheServlet.java
@@ -0,0 +1,9 @@
+package com.construction.Web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+
+@WebServlet ("TacheServlet")
+public class TacheServlet extends HttpServlet {
+}
