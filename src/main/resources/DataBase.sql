7ace1562a2ef2a2c20788867025ddd805e786346
Youssef Ait Rais <copybuisness007@gmail.com>
2025-03-21 16:02:32 +0100

    database

--- src/main/resources/DataBase.sql
+++ src/main/resources/DataBase.sql
@@ -22,7 +22,7 @@ create table tache (
                        description text not null,
                        date_debut date not null,
                        date_fin date not null,
-                       foreign key (id_projet) references projet (id_projet)
+                       foreign key (id_projet) references projet (id_projet) on delete cascade
 );
 
 create table ressource (
@@ -32,6 +32,8 @@ create table ressource (
                            quantite int not null,
                            fournisseur varchar(400)  not null,
                            id_tache int not null,
-                           foreign key (id_tache) references tache(id_tache)
+                           foreign key (id_tache) references tache(id_tache) on delete cascade
 );
 
+insert into projet (id_projet , nom , description , date_debut , date_fin , budget) values (1 , 'youssef' , 'aitrais' , '2024-10-20' , '2025-10-20' , 10000 );
+select * from tache;
